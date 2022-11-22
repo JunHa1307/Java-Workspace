f692296226f6e7a40555f9dcd1278598dffcf129
JunHa1307 <117423310+JunHa1307@users.noreply.github.com>
2022-11-22 15:16:02 +0900

    12일차

--- Homework2_OOP/src/com/kh/hw/shape/view/ShapeMenu.java
+++ Homework2_OOP/src/com/kh/hw/shape/view/ShapeMenu.java
@@ -46,6 +46,34 @@ public class ShapeMenu {
 		System.out.print("메뉴 번호 : ");
 		int menuNum = sc.nextInt();
 		if(menuNum == 1 || menuNum == 2 || menuNum == 3 || menuNum == 9) {
+			inputSize(3,menuNum);
+		}else {	
+			System.out.println("잘못된 번호입니다. 다시 입력하세요.");
+			triangleMenu();
+		}
+		triangleMenu();
+	}
+	
+	public void squreMenu() {
+		System.out.println("===== 사각형 =====");
+		System.out.println("1. 사각형 둘레");
+		System.out.println("2. 사각형 면적");
+		System.out.println("3. 사각형 색칠");
+		System.out.println("4. 사각형 정보");
+		System.out.println("9. 메인으로");
+		System.out.print("메뉴 번호 : ");
+		int menuNum = sc.nextInt();
+		if(menuNum == 1 || menuNum == 2 || menuNum == 3 || menuNum == 4 || menuNum == 9) {
+			inputSize(4,menuNum);
+		}else {
+			System.out.println("잘못된 번호입니다. 다시 입력하세요.");
+			squreMenu();
+		}
+		squreMenu();
+	}
+	
+	public void inputSize(int type, int menuNum) {
+		if(type == 3) {
 			switch(menuNum) {
 				 case 1 :{
 					System.out.print("높이 : ");
@@ -68,23 +96,7 @@ public class ShapeMenu {
 					inputMenu();
 				}
 			}
-		}else {	
-			System.out.println("잘못된 번호입니다. 다시 입력하세요.");
-			triangleMenu();
-		}
-		triangleMenu();
-	}
-	
-	public void squreMenu() {
-		System.out.println("===== 사각형 =====");
-		System.out.println("1. 사각형 둘레");
-		System.out.println("2. 사각형 면적");
-		System.out.println("3. 사각형 색칠");
-		System.out.println("4. 사각형 정보");
-		System.out.println("9. 메인으로");
-		System.out.print("메뉴 번호 : ");
-		int menuNum = sc.nextInt();
-		if(menuNum == 1 || menuNum == 2 || menuNum == 3 || menuNum == 4 || menuNum == 9) {
+		}else {
 			switch(menuNum) {
 				 case 1 :{
 					System.out.print("높이 : ");
@@ -114,15 +126,7 @@ public class ShapeMenu {
 					inputMenu();
 				}
 			}
-		}else {
-			System.out.println("잘못된 번호입니다. 다시 입력하세요.");
-			squreMenu();
 		}
-		squreMenu();
-	}
-	
-	public void inputSize(int type, int menuNum) {
-		
 	}
 	
 	public void printInformation(int type) {
