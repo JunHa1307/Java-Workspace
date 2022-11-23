50f91e7f07c90677e52d24e1c627b6dfa155cbd0
JunHa1307 <117423310+JunHa1307@users.noreply.github.com>
2022-11-23 09:18:46 +0900

    Update ObjectArrayRun.java

--- 07_ObjectArray/src/com/kh/chap01_oneVsmany/run/ObjectArrayRun.java
+++ 07_ObjectArray/src/com/kh/chap01_oneVsmany/run/ObjectArrayRun.java
@@ -39,12 +39,23 @@ public class ObjectArrayRun {
 		System.out.print("검색할 책의 제목 : ");
 		String searchTitle = sc.nextLine();
 		
+//		for(int i = 0; i < arr.length; i++) {
+//			if(arr[i].getTitle().equals(searchTitle)) {
+//				System.out.println(arr[i].getPrice());
+//			}
+//		}
+		
+//		 만약 일치하는 도서를 찾지 못한경우 "검색된 도서가 없습니다" 출력
+		
 		for(int i = 0; i < arr.length; i++) {
 			if(arr[i].getTitle().equals(searchTitle)) {
 				System.out.println(arr[i].getPrice());
+			}else {
+				System.out.println("검색된 도서가 없습니다.");
 			}
 		}
 		
 	}
-
+	
+	
 }
