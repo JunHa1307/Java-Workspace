dc175bf8f2e7697d18dc6911d87c10198216cf5c
wnsgk1307 <wnsgk1207@gmail.com>
2022-12-08 15:16:28 +0900

    lamda

--- 13_Collection/src/com/kh/practice/map/controller/MemberController.java
+++ 13_Collection/src/com/kh/practice/map/controller/MemberController.java
@@ -1,30 +1,55 @@
 package com.kh.practice.map.controller;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeMap;
 
 import com.kh.practice.map.model.vo.Member;
 
 public class MemberController {
 
-	private HashMap map = new HashMap();
+	private HashMap<String,Member> map = new HashMap<String,Member>();
 	
 	public boolean joinMembership(String id, Member m) {
-		return true;
+		if(map.get(id) == null) {
+			map.put(id, m);
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
-	public String login(String id, String password) {
-		return "";
+	public String logIn(String id, String password) {
+		String name = null;
+		if(map.get(id)!=null && map.get(id).getPassword().equals(password)) {
+			name = map.get(id).getName();
+		}
+		return name;
 	}
 	
 	public boolean changePassword(String id, String oldPw, String newPw) {
-		return true;
+		if(map.get(id)!=null && map.get(id).getPassword().equals(oldPw)) {
+			map.get(id).setPassword(newPw);
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
-	public void changName(String id, String newName) {
-		
+	public void changeName(String id, String newName) {
+			map.get(id).setName(newName);
 	}
 	
-	public TreeMap snaeName(String name) {
-		return;
+	
+	
+	public TreeMap sameName(String name) {
+		Set<String> set = map.keySet();
+		TreeMap<String,String> tm = new TreeMap();
+		for(String key : set) {
+			if(map.get(key).getName().equals(name)) {
+				tm.put(key,name);
+			}
+		}
+		return tm;
 	}
 }
